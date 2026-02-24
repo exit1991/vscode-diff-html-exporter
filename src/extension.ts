b44d8db4151c30632ebf5ac913ceeeed0cc6391e
hdeg <76611318+exit1991@users.noreply.github.com>
2026-02-24 22:18:12 +0900

    基本機能を実装

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { createTwoFilesPatch } from "diff";
+import { html } from "diff2html";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    "vscode-diff-html-exporter.exportHtml",
+    async () => {
+      const tab = vscode.window.tabGroups.activeTabGroup.activeTab;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-diff-html-exporter" is now active!');
+      if (!(tab?.input instanceof vscode.TabInputTextDiff)) {
+        vscode.window.showErrorMessage("Diff画面で実行してください");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-diff-html-exporter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-diff-html-exporter!');
-	});
+      const leftUri = tab.input.original;
+      const rightUri = tab.input.modified;
 
-	context.subscriptions.push(disposable);
+      const leftDoc = await vscode.workspace.openTextDocument(leftUri);
+      const rightDoc = await vscode.workspace.openTextDocument(rightUri);
+
+      const leftText = leftDoc.getText();
+      const rightText = rightDoc.getText();
+
+      const patch = createTwoFilesPatch(
+        leftUri.path,
+        rightUri.path,
+        leftText,
+        rightText
+      );
+
+      const htmlOutput = html(patch, {
+        drawFileList: true,
+        matching: "lines",
+        outputFormat: "side-by-side"
+      });
+
+      const uri = await vscode.window.showSaveDialog({
+        filters: { HTML: ["html"] },
+        saveLabel: "Export Diff HTML"
+      });
+
+      if (!uri) {
+        return;
+      }
+
+      await vscode.workspace.fs.writeFile(uri, Buffer.from(htmlOutput));
+
+      vscode.window.showInformationMessage("HTMLを出力しました");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
